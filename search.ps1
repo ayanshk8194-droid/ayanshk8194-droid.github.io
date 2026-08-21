0888c9d9860c3914bded6ac57e57a8b1c02dd901
ayanshk8194-droid <ayan.don8149@gmail.com>
2026-08-21 11:34:23 +0530

    Remove temp script

--- search.ps1
+++
@@ -1,7 +0,0 @@
-$text = [System.IO.File]::ReadAllText("app.bundle.js")
-$pattern = '.{0,150}\}\)\)\).{0,150}'
-$ms = [regex]::Matches($text, $pattern)
-foreach ($m in $ms) {
-    Write-Output "---"
-    Write-Output $m.Value
-}
